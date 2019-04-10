b11110991b4e98c0df2f79ae9f21a7eea1ae9821
Max Tang <tw7613781@gmail.com>
2019-04-10 15:58:44 +0900

    WIP: init and rsi

---
+++ indicator.py
@@ -0,0 +1,11 @@
+import numpy
+
+def rsi(prices, n=14):
+    '''
+    params:
+        prices: python list type, close price of list of time series candles
+        n: rsi params, default is 14
+    return:
+        rsi: python list type, rsi value of prices
+    '''    
+    pass
