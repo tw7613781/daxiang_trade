a6540e40b4a4a2eaddc306a28e7db8cab8b553cb
Max Tang <tw7613781@gmail.com>
2019-04-24 17:01:55 +0900

    feat: system skeletono

---
+++ settings.py
@@ -0,0 +1,19 @@
+########################################################################################################################
+# Connection/Auth
+########################################################################################################################
+
+# API URL.
+BASE_URL = "https://testnet.bitmex.com/api/v1/"
+# BASE_URL = "https://www.bitmex.com/api/v1/" # Once you're ready, uncomment this.
+
+# The BitMEX API requires permanent API keys. Go to https://testnet.bitmex.com/app/apiKeys to fill these out.
+API_KEY = "faQczjkhb9UQ5nv09KTjyTRQ"
+API_SECRET = "God1eB-ywL0CfhXwflkyfcB9z7XV36sbwss_JkEvf1RQqF2E"
+
+
+########################################################################################################################
+# Target
+########################################################################################################################
+
+# Instrument to market make on BitMEX.
+SYMBOL = "XBTUSD"
