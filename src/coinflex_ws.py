0eb21c50c14987304d8e13c6630cda5d68fa6d3d
Tang Wei <tw7613781@gmail.com>
2022-01-25 09:58:16 +0800

    - only subsribe specific order msg - handle resetByPeer error

--- src/coinflex_ws.py
+++ src/coinflex_ws.py
@@ -7,12 +7,13 @@ import json
 import os
 from datetime import datetime
 from dotenv import load_dotenv
-from utils import current_milli_ts, TERM_BLUE, TERM_NFMT
+from utils import current_milli_ts, TERM_BLUE, TERM_NFMT, TERM_GREEN, TERM_RED
 
 load_dotenv()
 
 api_key = os.getenv('APIKEY')
 api_secret = os.getenv('APISECRET')
+user_id = os.getenv('USERID')
 
 sell_price = float(os.getenv('SELLPRICE'))
 buy_price = float(os.getenv('BUYPRICE'))
@@ -40,10 +41,10 @@ def subscribe_balance_msg():
     'tag': 101
   }
 
-def subscribe_orders_msg():
+def subscribe_orders_msg(market):
   return {
     'op': 'subscribe', 
-    'args': ['order:all'], 
+    'args': [f'order:{market}'], 
     'tag': 102
   }
 
@@ -69,32 +70,44 @@ def place_limit_order_msg(market, side, quantity, price):
     }
   }
 
+async def initial_conn(ws):
+  await ws.send(json.dumps(auth_msg()))
+  await ws.send(json.dumps(subscribe_orders_msg('FLEX-USD')))
+  await ws.send((json.dumps(subscribe_ticker_msg('FLEX-USD'))))
+  # await ws.send(json.dumps(place_limit_order_msg('FLEX-USD', 'BUY', 1, 4.5)))
+
 async def process():
   async with websockets.connect(websocket_endpoint) as ws:
-    await ws.send(json.dumps(auth_msg()))
-    await ws.send(json.dumps(subscribe_orders_msg()))
-    await ws.send((json.dumps(subscribe_ticker_msg('FLEX-USD'))))
-    # await ws.send(json.dumps(place_limit_order_msg('FLEX-USD', 'BUY', 1, 4.5)))
-    while ws.open:
-      resp = await ws.recv()
-      msg = json.loads(resp)
-      # print(msg)
-      if 'event' in msg and msg['event']=='login':
-        ts = msg['timestamp']
-        print(f'{TERM_BLUE}Login succeed at {datetime.fromtimestamp(int(ts) / 1000)}{TERM_NFMT}')
-      if 'table' in msg and msg['table']=='order':
-        data = msg['data']
-        print(f'{TERM_BLUE}{data}{TERM_NFMT}')
-        if 'notice' in data and data['notice'] == 'OrderMatched':
-          side = data['side']
-          quantity = data['quantity']
-          price = data['price']
-          if side == 'BUY':
-            # 买单成交了,要挂卖单
-            await ws.send(json.dumps(place_limit_order_msg('FLEX-USD', 'SELL', int(quantity * price / sell_price), sell_price)))
-          elif side == 'SELL':
-            # 卖单成交了,要挂买单
-            await ws.send(json.dumps(place_limit_order_msg('FLEX-USD', 'BUY', int(quantity * price / buy_price), buy_price)))
+    try: 
+      await initial_conn(ws)
+      while ws.open:
+        resp = await ws.recv()
+        msg = json.loads(resp)
+        # print(msg)
+        if 'event' in msg and msg['event']=='login':
+          ts = msg['timestamp']
+          print(f'{TERM_GREEN}Login succeed at {datetime.fromtimestamp(int(ts) / 1000)}{TERM_NFMT}\n')
+        if 'table' in msg and msg['table']=='order':
+          data = msg['data']
+          print(f'{TERM_BLUE}{data}{TERM_NFMT}')
+          if 'notice' in data and data['notice'] == 'OrderMatched':
+            print(f'{TERM_BLUE}Execute order...{TERM_NFMT}')
+            side = data['side']
+            quantity = float(data['quantity'])
+            price = float(data['price'])
+            if side == 'BUY':
+              # 买单成交了,要挂卖单
+              print(f'{TERM_BLUE}Sell order...{quantity} - {sell_price}{TERM_NFMT}\n')
+              await ws.send(json.dumps(place_limit_order_msg('FLEX-USD', 'SELL', quantity, sell_price)))
+            elif side == 'SELL':
+              # 卖单成交了,要挂买单
+              print(f'{TERM_BLUE}Bull order...{int(quantity * price / buy_price)}{buy_price}{TERM_NFMT}\n')
+              await ws.send(json.dumps(place_limit_order_msg('FLEX-USD', 'BUY', int(quantity * price / buy_price), buy_price)))
+    except ConnectionResetError:
+      print(f'{TERM_RED}Connection reset by peer, try to reconnect...{TERM_NFMT}')
+      await process()
+
 
 if __name__ == '__main__':
+  print(f'{TERM_GREEN}Config loaded, user: {user_id}, buy_price: {buy_price}, sell_price: {sell_price}{TERM_NFMT}')
   asyncio.get_event_loop().run_until_complete(process())
